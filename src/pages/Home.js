393628c187fe7644742fe44a0b66d2ee5696cb4c
thao <thaole0325@gmail.com>
2023-08-20 09:09:38 -0400

    added loading state for fetching data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import BlogList from "../components/BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
+  // set to `true`, data is loading at first while we make request
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
@@ -11,11 +14,16 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        // once we get data, change `loading` state to false.
+        setLoading(false); // NOT loading
       });
   }, []);
 
   return (
     <div className="home">
+      {/* conditional check for loading state */}
+      {loading && <div>Loading...</div>}
+
       {/* render BlogList once we have blogs data */}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
