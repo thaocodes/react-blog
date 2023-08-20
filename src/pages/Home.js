2ebf91c9f1062e78be2f071acb31907e49656f0e
thao <thaole0325@gmail.com>
2023-08-20 12:17:34 -0400

    error handling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,29 @@ const Home = () => {
 
   // set to `true`, data is loading at first while we make request
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          // if error, we go right to .catch block
+          throw Error("Could not fetch data"); // creating a new Error object with this message
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
-        // once we get data, change `loading` state to false.
         setLoading(false); // NOT loading
+        setError(null); // no error if data
+      })
+      // catches the error thrown & names it `err`
+      // `err` is an instance of the Error object
+      .catch((err) => {
+        setLoading(false);
+        // get message from error
+        setError(err.message); // message is a property of Error
+        console.log(err.message);
       });
   }, []);
 
@@ -23,6 +36,8 @@ const Home = () => {
     <div className="home">
       {/* conditional check for loading state */}
       {loading && <div>Loading...</div>}
+
+      {error && <div>{error}</div>}
 
       {/* render BlogList once we have blogs data */}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
