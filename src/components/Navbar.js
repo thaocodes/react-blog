af4e12b16e8d63c7050f43927800eac3af2f68b5
thao <thaole0325@gmail.com>
2023-08-20 08:44:06 -0400

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+// arrow function
+
+const Navbar = () => {
+  return (  
+    <nav className="navbar">
+      <h1>Thao's Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
