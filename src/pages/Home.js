67e2c990c31a6d66942da31d36eebccb47394a52
thao <thaole0325@gmail.com>
2023-08-22 12:32:37 -0400

    refactored Home to use useFetch hook to make get all blogs request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,22 @@
-import { useState, useEffect } from "react";
 import BlogList from "../components/BlogList";
+import useFetch from "../hooks/useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
+  // DESTRUCTURE useFetch hook we created
+  // Need to pass in endpoint here as a parameter! 
+  const { data: blogs, loading, error } = useFetch('http://localhost:8000/blogs');
+  // grab the data: but name it `blogs` in this context
 
-  // set to `true`, data is loading at first while we make request
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          // if error, we go right to .catch block
-          throw Error("Could not fetch data"); // creating a new Error object with this message
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setLoading(false); // NOT loading
-        setError(null); // no error if data
-      })
-      // catches the error thrown & names it `err`
-      // `err` is an instance of the Error object
-      .catch((err) => {
-        setLoading(false);
-        // get message from error
-        setError(err.message); // message is a property of Error
-        console.log(err.message);
-      });
-  }, []);
-
+  //====  output data once we get it ==== 
   return (
     <div className="home">
       {/* conditional check for loading state */}
       {loading && <div>Loading...</div>}
-
       {error && <div>{error}</div>}
+
+      {/* here its called `data` that we get back but we're trying to still pass the blogs in  */}
+      {/* we could do blogs={data} here instead OR we could: */}
+      {/* NAME THE DATA BLOGS INSIDE THIS COMPONENT BY ADDING A : */}
 
       {/* render BlogList once we have blogs data */}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
