3b6374e43c0a3e92e41fd78faf1d43e11255228d
thao <thaole0325@gmail.com>
2023-08-23 11:08:09 -0400

    set up react-router for nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import Navbar from "./components/Navbar";
+import Create from "./pages/Create";
 import Home from "./pages/Home";
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // a component is a function, we must return something inside it (JSX template)
 // export the component to be used elsewhere
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* only 1 route shows at any 1 time, all routes go in here */}
+          {/* set up a route for each page */}
+          <Switch>
+            {/* need to add path property, nest component we want to visit inside the Route */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
