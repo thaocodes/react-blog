c02f59372012f76e1991f8b2b08fe56b69e97227
thao <thaole0325@gmail.com>
2023-08-23 18:52:28 -0400

    clean up function & abort controller for useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,7 +10,12 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
+    // store abort controller in const abortCont
+    const abortCont = new AbortController();
+
+    // associate aborController w/ fetch by adding 2nd arg to `fetch`
+    // use signal property
+    fetch(url, { signal: abortCont.signal })
       .then((response) => {
         if (!response.ok) {
           // if error, go right to .catch block
@@ -25,13 +30,22 @@ const useFetch = (url) => {
         setError(null); // no Error if we get data
       })
 
-      // catches the error thrown & names it `err`
+      // catches the error thrown & names it `err`, an instance of the Error object
       .catch((err) => {
-        // `err` is an instance of the Error object
-        // get message from error,  message is a property of Error
-        setError(err.message); // set Error state to be that Error message
-        setLoading(false);
+        // abort controller throws an error which gets caught here
+        if (err.name === "AbortError") {
+          console.log("Fetch Aborted");
+        } else {
+          // get message from error,  message is a property of Error
+          setError(err.message); // set Error state to be that Error message
+          setLoading(false);
+        }
       });
+
+    // clean up function to stop fetch
+    // aborts whatever fetch it's associated w/
+    return () => abortCont.abort(); // abort method
+    
     // url is a dependency
     // whenever url changes, rerun function to get data for that endpoint
   }, [url]);
