2dc915dabd35c1fbeec7b4b4be09b87ead97191c
thao <thaole0325@gmail.com>
2023-08-23 20:09:09 -0400

    reuse useFetch custom hook to fetch blog details

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const BlogDetails = () => {
   // destructure route param using useParams hook
-  const { id } = useParams;
+  // grabs `id` from url
+  const { id } = useParams();
+  const {
+    data: blog,
+    loading,
+    error,
+  } = useFetch("http://localhost:8000/blogs" + id);
 
   return (
     <div className="blog-details">
-      {/* just outputting blog's id here  */}
-      <h2>Blog Details - {id}</h2>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+
+      {blog && (
+        <article>
+          <h2>{blog.title} </h2>
+          <p>Written by {blog.author} </p>
+          {/* body is a property, displays actual content of the blog */}
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
