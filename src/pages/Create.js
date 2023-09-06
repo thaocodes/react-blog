6c243620c5c19a451f6b925ab24ab3dae105ce0e
thao <thaole0325@gmail.com>
2023-09-06 11:03:00 -0400

    added input fields for adding new blog

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,26 @@
 const Create = () => {
+  
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      {/* add controlled inputs (web forms) */}
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+        />
+        <label>Blog author:</label>
+        <select>
+          <option value="thao">thao</option>
+          <option value="fresno">fresno</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
