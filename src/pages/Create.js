4f15fbf5e9f61c76525c87e95fc711db50fc76e1
thao <thaole0325@gmail.com>
2023-09-06 11:39:37 -0400

    added states to track input field changes for blog title, body, author

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 const Create = () => {
-  
+  // states for all input fields to track changing values
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("thao");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
@@ -9,17 +15,29 @@ const Create = () => {
         <input
           type="text"
           required
+          // set value to initial state
+          value={title}
+          // to track when input value changes & to update it, use onChange event
+          // set to anon arrow func which invokes setTitle
+          // onChange gives us access to event object
+          onChange={(e) => setTitle(e.target.value)} // target = input field, value = what's typed in
         />
         <label>Blog body:</label>
         <textarea
           required
-        />
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog author:</label>
-        <select>
+        {/* drop down options to select */}
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="thao">thao</option>
           <option value="fresno">fresno</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
   );
