940f0bc6c322a131ddef2414b93385338acc7661
thao <thaole0325@gmail.com>
2023-09-06 15:40:06 -0400

    fixed errors so blog details shows

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
 
 // destructuring blogs & title props passed down from `Home`
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs }) => {
   return (
     <div className="blog-list">
-      <h2>{title} </h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           {/* access each blog with map, each blog object has an `id` */}
-          <Link to={`/blogs/${blogs.id}`}>
+          <Link to={`/blogs/${blog.id}`}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
           </Link>
